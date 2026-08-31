3c393d12fb7583b01b0148aa915075d7f9a52ea6
Mohamed Morsi <777medooo@gmail.com>
2026-08-31 06:56:59 +0300

    feat: expand RunTypesDemo with additional type demonstrations and parsing examples

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// ============ Part A ====================/
+﻿// ============ Part A ====================//
 
 // .csproj is the file that contain project configs like .net version, packages and dependencies.
 // Program.cs is the entry point of our application.
@@ -10,14 +10,109 @@ namespace CSharpBasicsAssignment;
 // File-scoped namespace removes the need for a namespace block,
 // so the code inside the namespace doesn't need an extra indentation level.
 
+// My project use .sln but .slnx is better in solving git conflicts, also readable and simpler than .sln .
+
+
 class Program
 {
   static void Main()
   {
-    Console.WriteLine("Hello");
+    
   }
+
+  // ============ Part B ====================//
+  static void RunTypesDemo()
+  {
+    // 1.
+    int num = 10;
+    long bigNum = 100L;
+    double price = 10.5;
+    decimal salary = 5000.50m;
+    bool isActive = true;
+    char grade = 'A';
+    string name = "Mohamed";
+    var age = 23;
+
+    Console.WriteLine($"{num} - {num.GetType()}");
+    Console.WriteLine($"{bigNum} - {bigNum.GetType()}");
+    Console.WriteLine($"{price} - {price.GetType()}");
+    Console.WriteLine($"{salary} - {salary.GetType()}");
+    Console.WriteLine($"{isActive} - {isActive.GetType()}");
+    Console.WriteLine($"{grade} - {grade.GetType()}");
+    Console.WriteLine($"{name} - {name.GetType()}");
+    Console.WriteLine($"{age} - {age.GetType()}");
+
+    // 2.
+      // Implicit:
+    int x = 10;
+    long y = x;
+
+    char c = 'A';
+    int asciiCode = c;
+
+      // No Cast is needed because we convert from smaller to larger and C# consider this conversion doesn't cause conflict
+    Console.WriteLine(y);
+    Console.WriteLine(asciiCode);
+
+      // Explicit:
+    double value = 9.7;
+
+    int result1 = (int)value;
+    int result2 = Convert.ToInt32(value);
+
+      // int removes the fraction part, while Convert.ToInt32 rounds the value.
+    Console.WriteLine(result1);
+    Console.WriteLine(result2);
+
+      // Integer division
+    int a = 5 / 2;
+    double b = 5.0 / 2;
+
+    // a = 2 because we divide int/int, while double = 2.5 because we divide double/int
+
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+
+     // Boxing / Unboxing
+    int numBox = 42;
+
+    object obj = numBox;
+    Console.WriteLine(obj);
+
+    int unboxed = (int)obj;
+    Console.WriteLine(unboxed);
+
+      // Parsing
+    string input = "42";
+    int parsed = int.Parse(input);
+    Console.WriteLine(parsed);
+
+    string badInput = "abc";
+
+    if (int.TryParse(badInput, out int parsedBad))
+      Console.WriteLine(parsedBad);
+    else
+      Console.WriteLine("TryParse failed.");
+
+      // float to decimal
+    float f = 10.5f;
+
+    // decimal d = f; xxxxx
+    // float to decimal is not an implicit conversion, 
+    // because decimal is more precise than float and use base-10
+    // while float less precise and use base-2
+
+    decimal d = (decimal)f;
+    Console.WriteLine(d);
+  }
+
+
+
+
+
+  
 }
 
-// My project use .sln but .slnx is better in solving git conflicts, also readable and simpler than .sln .
 
-// ============ Part A ====================/
+
+
